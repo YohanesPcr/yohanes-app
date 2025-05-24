f9c16fa14f5e56e9081bb41d9bfd71e4e13553ec
Yohanes <yohanes23si@mahasiswa.pcr.ac.id>
2025-05-24 21:36:56 +0700

    pindah folder json ke admin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'  
-import react from '@vitejs/plugin-react'  
-import tailwindcss from '@tailwindcss/vite'  
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/  
-export default defineConfig({  
-  plugins: [  
-    react(),  
-    tailwindcss(),  
-  ]  
-})  
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@json': path.resolve(__dirname, './src/json'),
+    },
+  },
+});
